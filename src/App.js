3103d2b59c00c1ae4c6bbae6d95a89b29618fb79
taahahussainkhan <taaha128@gmail.com>
2023-03-25 16:39:41 -0700

    Modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
+
+const Container=styled.div`
+display:flex;
+flex-direction: column;
+align-items:center;
+margin:30px 0 10px;
+font-family:Montserrat;
+`;
+const Header=styled.span`
+color:black;
+font-size:25px;
+font-weight:bold;
+
+
+`
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container>
+    <Header>Expense Tracker</Header>
+  </Container>;
 }
 
 export default App;
